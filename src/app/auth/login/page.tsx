e2b479743808a854dd52ede61d10f72bc560b969
Mattias <duartemattias4@gmail.com>
2024-04-23 17:07:03 -0400

    initial ligth theme config

---
+++ src/app/auth/login/page.tsx
@@ -0,0 +1,10 @@
+import { Button } from "@nextui-org/button";
+
+export default function() {
+    return (
+        <div>
+            <h1>Hola mundo</h1>
+            <Button radius="lg" color="primary">Hola mundo</Button>
+        </div>
+    );
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,34 @@
+import { nextui } from "@nextui-org/react";
 import type { Config } from "tailwindcss";
+
+import { lightTheme } from "./src/config";
+
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/modules/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
-    },
+    extend: {}
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui({
+    defaultTheme: 'light',
+    layout: {
+
+      radius: {
+        small: '3px',
+        large: '9px',
+        medium: '6px',
+      },
+      
+    },
+    themes: {
+      ...lightTheme
+    }
+  })]
 };
 export default config;
